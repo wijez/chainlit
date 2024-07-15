8400817be915aac0320a8aa507991fad991faad2
VIET-GITL <viet.info.43@gmail.com>
2024-07-15 12:08:54 +0700

    label topic insign

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ settings = {
     "model": "gpt-3.5-turbo",
     "temperature": 0,
 }
-topic = input("Filter (python , postgreSQL, fastapi, Django, AI): ")
+topic = ["python", "postgreSQL", "fastapi", "Django", "AI"]
 prompt = f"You are an expert in {topic} and answer questions related to them., you always reply in Vietnamese"
 
 
@@ -29,3 +29,17 @@ async def on_message(message: cl.Message):
         **settings
     )
     await cl.Message(content=response.choices[0].message.content).send()
+
+
+@cl.set_starters
+async def starters():
+    starters_list = []
+    for _ in topic:
+        starters_list.append(
+            cl.Starter(
+                label=f"/{_} help",
+                message=f"Can I help you about {_}?",
+            )
+        )
+    return starters_list
+
